e2118d6372239a22a416d5e2b01555c4a7f532d2
José Riobueno <jrrv99@gmail.com>
2023-12-07 15:48:42 -0400

    feat(Tablero): :sparkles: add tablero initial config

--- LineasRectangulosColores.java
+++ LineasRectangulosColores.java
@@ -1,10 +1,127 @@
-import java.awt.Font;
-import java.util.Scanner;
+import java.util.Random;
+// import java.awt.Font;
+// import java.util.Scanner;
 
 public class LineasRectangulosColores {
     /**
-     * Panel
+     * Enum for objects representation in array indexes
+     * constantes para la representacion de los objetos 
+     * TODO: Use Enum DSE for good practices
+     * TODO: CANTIDAD_DE_OBJS_EXISTENTES = Objetos.values().length
      */
+    public static final int EMPTY_SLOT = -1; // Cuadro vacio en el tablero
+    public static final int CUADRADO = 0;
+    public static final int BOLA_ROJA = 1;
+    public static final int BOLA_AZUL = 2;
+    public static final int BOLA_VERDE = 3;
+    public static final int BOLA_NARANJA = 4;
+    public static final int BOLA_MAGENTA = 5;
+    public static final int BOLA_AMARILLA = 6;
+    public static final int CANTIDAD_DE_OBJS_EXISTENTES = 7; // cantidad de objetos cuadrado mas 6 circulos
+
+    public static final int DEFAULT_PROXIMOS_OBJETOS_CANTIDAD = 3; // cantidad de objetos a agregar
+
+
+    /*****************************************************************/
+    /*************************** UTILITIES ***************************/
+    /*****************************************************************/
+
+    // Retorna un entero entre 0 y n es decir [0, n]
+    /*@ requires 0 < n <= Integer.MAX_VALUE;
+      @ ensures 0 <= \result <= n;
+      @*/
+    public static /*@ pure @*/ int getRandomInt(int n) {
+        Random random = new Random();
+        return random.nextInt(n + 1);
+    }
+
+    /*****************************************************************/
+    /**************************** TABLERO ****************************/
+    /*****************************************************************/
+	public static final int DEFAULT_TABLERO_SIZE = 9;
+	public static int[][] tablero = new int[DEFAULT_TABLERO_SIZE][DEFAULT_TABLERO_SIZE];
+
+    /**
+     * Representa la cantidad de cada objeto en el tablero, y el indice 
+     * corresponde al valor del objeto representado en las constantes
+     */
+    public static int[] contador_de_objetos = {0, 0, 0, 0, 0, 0, 0};
+    public static int[] proximosObjetos = new int[3];
+
+    public static /*@ pure @*/ int obtenerObjEnMenorCantidad() {
+        int i = 1, menor = 0;
+
+        while (i < contador_de_objetos.length) {
+            if (contador_de_objetos[i] < contador_de_objetos[menor])
+                menor = i;
+            i++;
+        }
+
+        return menor;
+    }
+
+    // Procedimiento 2
+    public static /*@ pure @*/ void obtenerProximosObjetos() {
+        proximosObjetos[0] = getRandomInt(CANTIDAD_DE_OBJS_EXISTENTES - 1);  // -1 porque retorna [0, n]
+        proximosObjetos[1] = getRandomInt(CANTIDAD_DE_OBJS_EXISTENTES - 1);  // -1 porque retorna [0, n]
+        proximosObjetos[2] = obtenerObjEnMenorCantidad();
+    }
+
+    public static /*@ pure @*/ boolean esCasillaVacia(int fila, int columna) {
+        return tablero[fila][columna] == EMPTY_SLOT;
+    }
+
+    // TODO: improve algoritm performance
+    public static /*@ pure @*/ void obtenerCasillaRandomVacia(int[] casilla) {
+        int FILA = 0;
+        int COLUMNA = 1;
+
+        do {
+            casilla[FILA] = getRandomInt(DEFAULT_TABLERO_SIZE - 1);  // -1 porque retorna [0, n]
+            casilla[COLUMNA] = getRandomInt(DEFAULT_TABLERO_SIZE - 1);  // -1 porque retorna [0, n]
+        } while (!esCasillaVacia(casilla[FILA], casilla[COLUMNA]));
+    }
+
+    public static /*@ pure @*/ void aumentarCantidadDeObjeto(int objeto, int cantidad) {
+        contador_de_objetos[objeto] += cantidad;
+    }
+
+    public static /*@ pure @*/ void agregarProximosObjetos() {
+        int FILA = 0;
+        int COLUMNA = 1;
+        int i = 0;
+        int[] casilla = new int[2];
+
+        while (i < DEFAULT_PROXIMOS_OBJETOS_CANTIDAD) {
+            obtenerCasillaRandomVacia(casilla);
+            // Colocar el objeto en la casilla vacia al azar
+            tablero[casilla[FILA]][casilla[COLUMNA]] = proximosObjetos[i];
+            // aumentar la cantidad del objeto que agrega en el tablero
+            aumentarCantidadDeObjeto(proximosObjetos[i], 1);
+
+            i++;
+        }
+    }
+
+    // Procedimiento 1
+	public static /*@ pure @*/ void inicializarTablero() {
+        int fila = 0, columna;
+
+        while (fila < DEFAULT_TABLERO_SIZE) {
+            columna = 0;
+            while (columna < DEFAULT_TABLERO_SIZE) {
+                tablero[fila][columna] = EMPTY_SLOT;
+                columna++;
+            }
+            fila++;
+        }
+        obtenerProximosObjetos();
+        agregarProximosObjetos();
+    }
+
+    /*****************************************************************/
+    /***************************** PANEL *****************************/
+    /*****************************************************************/
     public static final int PANEL_BARRA_TITULO_ALTURA = 38; // alto de la barra de titulo 
     public static final int PANEL_DEFAULT_XMAX = 512;
     public static final int PANEL_DEFAULT_YMAX = 512;
@@ -27,11 +144,14 @@ public class LineasRectangulosColores {
     }
 
     public static void main(String[] args) {
-        MaquinaDeTrazados panel = new MaquinaDeTrazados(
-            PANEL_XMAX,
-            normalizedPanelYMAX(),
-            PANEL_DEFAULT_TITLE,
-            Colores.LIGHT_GRAY
-        );
+        // MaquinaDeTrazados panel = new MaquinaDeTrazados(
+        //     PANEL_XMAX,
+        //     normalizedPanelYMAX(),
+        //     PANEL_DEFAULT_TITLE,
+        //     Colores.LIGHT_GRAY
+        // );
+        inicializarTablero();
+
+        Utils.imprimirTableroPorConsola(tablero, DEFAULT_TABLERO_SIZE);
     }
 }
---
+++ Utils.java
@@ -0,0 +1,105 @@
+import java.util.Random;
+import java.awt.Font;
+
+public class Utils {
+    //Retorna el numero divisible por M mas cercano a N
+    /*@ requires 0 < N <= Integer.MAX_VALUE;
+      @ requires 0 < M <=  Integer.MAX_VALUE;
+      @ requires M <= N;
+      @*/
+    public static /*@ pure @*/ int NormalizeToExactlyDivisible(int N, int M) {
+        int divisible = (int)Math.round(N / M) * M;
+        return divisible;
+    }
+
+    /*@ requires plt != null;
+      @ requires 0 <= x <= plt.XMAX < Integer.MAX_VALUE;
+	  @ requires 0 <= y <= plt.YMAX < Integer.MAX_VALUE;
+	  @ requires l < Integer.MAX_VALUE;
+      @ ensures true;
+      @*/
+    public static /*@ pure @*/ void dibujarLineaHorizontal(MaquinaDeTrazados plt, int x, int y, int l, Colores color) {
+        plt.dibujarLinea(
+            x, y,
+            x + l, y,
+            color
+        );
+        plt.mostrar();
+    }
+
+    /*@ requires plt != null;
+      @ requires 0 <= x <= plt.XMAX < Integer.MAX_VALUE;
+	  @ requires 0 <= y <= plt.YMAX < Integer.MAX_VALUE;
+	  @ requires l < Integer.MAX_VALUE;
+      @ ensures true;
+      @*/
+    public static /*@ pure @*/ void dibujarLineaVertical(MaquinaDeTrazados plt, int x, int y, int l, Colores color) {
+        plt.dibujarLinea(
+            x, y,
+            x, y + l,
+            color
+        );
+        plt.mostrar();
+    }
+
+    public static /*@ pure @*/ void dibujarReglasDeCuadricula(MaquinaDeTrazados plt, int x, int y, int size, int SLOTS_NUMBER) {
+        int i = 0;
+        int NORMALIZED_SIZE = NormalizeToExactlyDivisible(size, SLOTS_NUMBER);
+        int LINE_WIDTH = 1;
+        int SLOT_WIDTH = NORMALIZED_SIZE / SLOTS_NUMBER;
+
+		while(i < SLOTS_NUMBER) {
+            dibujarLineaHorizontal(plt, x - SLOT_WIDTH / 2, y + SLOT_WIDTH * i + SLOT_WIDTH / 2, NORMALIZED_SIZE + SLOT_WIDTH, Colores.RED);
+            dibujarLineaVertical(plt, x + SLOT_WIDTH * i + SLOT_WIDTH / 2, y - SLOT_WIDTH / 2, NORMALIZED_SIZE + SLOT_WIDTH, Colores.RED);
+            i++;
+		}
+	}
+
+    public static /*@ pure @*/ void dibujarIndices(MaquinaDeTrazados plt, int x, int y, int size, int SLOTS_NUMBER) {
+        int i = 0;
+        int NORMALIZED_SIZE = NormalizeToExactlyDivisible(size, SLOTS_NUMBER);
+        int LINE_WIDTH = 1;
+        int SLOT_WIDTH = NORMALIZED_SIZE / SLOTS_NUMBER;
+
+        plt.configurarFuente("SansSerif", Font.PLAIN, 12);
+
+		while(i < SLOTS_NUMBER) {
+            // Numero horizontal del slot
+            plt.dibujarString(Integer.toString(i), x + SLOT_WIDTH * i + (SLOT_WIDTH / 2) - 4, y - (SLOT_WIDTH / 2) + 5);
+            
+            // Pintar numero vertical
+            plt.dibujarString(Integer.toString(i), x - (SLOT_WIDTH / 2), y + SLOT_WIDTH * i + (SLOT_WIDTH / 2) + 5);
+		}
+	}
+
+	public static /*@ pure @*/ void printGrid(MaquinaDeTrazados plt, int x, int y, int size, int SLOTS_NUMBER) {
+        int i = 0;
+        int NORMALIZED_SIZE = NormalizeToExactlyDivisible(size, SLOTS_NUMBER);
+        int LINE_WIDTH = 1;
+        int SLOT_WIDTH = NORMALIZED_SIZE / SLOTS_NUMBER;
+
+		while(i < SLOTS_NUMBER + 1) {
+            // Linea Horizontal del grid
+			dibujarLineaHorizontal(plt, x, y + SLOT_WIDTH * i, NORMALIZED_SIZE, Colores.BLACK);
+            // Linea Vertical del grid
+			dibujarLineaVertical(plt, x + SLOT_WIDTH * i, y, NORMALIZED_SIZE, Colores.BLACK);
+            i++;
+		}
+	}
+
+    public static void imprimirTableroPorConsola(int[][] tablero, int size) {
+        int fila = 0, columna;
+
+        while (fila < size) {
+            columna = 0;
+            System.out.print("| ");
+            while (columna < size) {
+                System.out.print(
+                (tablero[fila][columna] != -1 ? tablero[fila][columna] : " ")
+                + (columna < size - 1 ? " | " : " |\n"));
+                columna++;
+            }
+            fila++;
+        }
+    }
+} 
